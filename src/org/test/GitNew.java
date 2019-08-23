29701290f71383b80d3e54637a4a303f4df0e269
Hariraj15 <hariraj.mv@gmail.com>
2019-08-23 13:30:59 +0530

    Little Modified

--- src/org/test/GitNew.java
+++ src/org/test/GitNew.java
@@ -2,6 +2,7 @@ package org.test;
 
 public class GitNew {
 	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\asus\\eclipse-HariJava\\Hariraj22\\NEDriver\\chromedriver.exe");
 		
 	}
 
